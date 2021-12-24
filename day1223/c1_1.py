4842f01f827b931997cff81bfe949a140e22d679
jun <watanabejapan2trap@gmail.com>
2021-12-24 10:58:52 +0900

    c1_1は試しに修正。Code1_10はjavaの作成。c1_14は学習

--- day1223/c1_1.py
+++ day1223/c1_1.py
@@ -1,2 +1,3 @@
 print(1)
 print(10)
+print(11)
---
+++ day1224/Code1_10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Code1_10{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("あなたの名前を入力>>");
+		String name=sc.nextLine();
+		System.out.printf("おお！%sが来るのを待っておったぞ！",name);
+	}
+}
+
