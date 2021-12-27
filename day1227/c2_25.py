2d852bd8cb0043e8df19356d71bd92d431c26fbb
jun <watanabejapan2trap@gmail.com>
2021-12-27 13:49:57 +0900

    c2_25追加 ディクショナリの中にセットをネスト

---
+++ day1227/c2_25.py
@@ -0,0 +1,10 @@
+member_hobbies={
+        '松田':{'SNS','麻雀','自転車'},
+        '麻木':{'麻雀','食べ歩き','数学','数学','数学',},
+}
+#全員の趣味一覧を表示
+print(member_hobbies)
+#松田君の趣味一覧を表示
+print(member_hobbies['松田'])
+#麻木さんの趣味一覧を表示
+print(member_hobbies['麻木'])
