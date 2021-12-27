7afc7b1504d08b271b59dbce9e7323d8347da595
jun <watanabejapan2trap@gmail.com>
2021-12-27 10:11:57 +0900

    いろいろ修正

---
+++ day1224/ex01.py
@@ -0,0 +1,8 @@
+name=input('あなたの犬の名前>>')
+age=input('あなたの犬の年齢>>')
+print(f'{name}{age}歳は人間換算で{int(age)*7}です')
+
+
+word=input('単語を入力>>')
+rWord=word[::-1]
+print(f'逆順にしました>{rWord}')
---
+++ day1224/rand.py
@@ -0,0 +1,10 @@
+#randomを利用するにはimportが必要
+import random
+
+
+n=random.randint(1,6)
+print(n)
+n=random.randint(1,6)
+print(n)
+n=random.randint(1,6)
+print(n)
