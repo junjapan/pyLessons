1b37ededaaadaabd174d42bc97e7a9afd1e8c6d6
jun <watanabejapan2trap@gmail.com>
2021-12-27 13:35:41 +0900

    c2_24.py追加 dectionaryのネスト

---
+++ day1227/c2_24.py
@@ -0,0 +1,7 @@
+matsuda_scores={'network':60,'database':80,'security':50}
+asagi_scores={'network':80,'database':75,'security':92}
+member_scores={
+        '松田':matsuda_scores,
+        '麻木':asagi_scores,
+}
+print(member_scores)
