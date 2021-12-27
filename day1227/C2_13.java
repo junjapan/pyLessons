949f6052c0ae103968eaa45ad91a3b0dc6026aed
jun <watanabejapan2trap@gmail.com>
2021-12-27 10:48:54 +0900

    C2_13.java追加 javaでハッシュマップの復習

---
+++ day1227/C2_13.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class C2_13{
+	public static void main(String[] args){
+		Map<String,Integer> scores=new HashMap<>();
+		scores.put("netword",60);
+		scores.put("database",80);
+		scores.put("security",50);
+		System.out.println(scores);
+	}
+}
