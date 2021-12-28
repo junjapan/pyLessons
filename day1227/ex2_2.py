70f35095cb075b650bba085bb1fbcda00ffa82f5
jun <watanabejapan2trap@gmail.com>
2021-12-28 11:43:04 +0900

    Ex2_2.py変更

--- day1227/ex2_2.py
+++ day1227/ex2_2.py
@@ -3,6 +3,6 @@ ma=int(input('算数の点数を入力>>'))
 sc=int(input('理科の点数を入力>>'))
 so=int(input('社会の点数を入力>>'))
 en=int(input('英語の点数を入力>>'))
-test={la,ma,sc,so,en}
+test=[la,ma,sc,so,en]
 print(f'合計点は{sum(test)}点です')
 print(f'平均点は{sum(test)/len(test)}点です')
