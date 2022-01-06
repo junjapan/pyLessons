296dc60fa16538eddc1f9f009495dc909b8df696
jun <watanabejapan2trap@gmail.com>
2022-01-06 11:35:41 +0900

    ex3_3,col1追加 if文演習、三項演算子

---
+++ day0106/col1.py
@@ -0,0 +1,8 @@
+import random
+n = random.randint(1,10)
+# let div = n % 2 == 0 ? "偶数":"奇数";
+div = '偶数' if n % 2== 0 else '奇数'
+print(f'{n}は{div}です')
+
+result='優' if n >= 9 else '良' if n >= 6 else '可' if n >= 3 else '不可'
+print(result)
---
+++ day0106/ex3_3.py
@@ -0,0 +1,20 @@
+isError = False
+n = 50
+if isError == False and n < 100:
+    print('ok')
+if n % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+aisatsu='こんにちは'
+
+if aisatsu == 'こんにちは':
+    print('ようこそ!')
+elif aisatsu == '景気は？':
+    print('ぼちぼちです')
+elif aisatsu == 'さようなら':
+    print('お元気で')
+else:
+    print('どうしました？')
+
