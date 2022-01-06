09b768aef9f8a3c1fb1e34cd4604a159572f63e9
jun <watanabejapan2trap@gmail.com>
2022-01-06 13:14:29 +0900

    c4_10.py追加 breakを使うと直近のループを抜ける javaもjsも同じ

---
+++ day0106/c4_10.py
@@ -0,0 +1,10 @@
+ages=[28,50,8,20,78,25,22,10,27,33]
+num=5
+print(ages)
+samples=list()
+for data in ages:
+    if 20 <= data < 30:
+        samples.append(data)
+        if len(samples) == num:
+            break
+print(samples)
