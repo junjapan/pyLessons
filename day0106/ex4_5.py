50e20f829c016933ca34da3c5b9464d0d5f83f52
jun <watanabejapan2trap@gmail.com>
2022-01-07 09:25:27 +0900

    演習問題追加、修正

--- day0106/ex4_5.py
+++ day0106/ex4_5.py
@@ -1,18 +1,32 @@
+# temp=[7.8,9.1,10.2,11.0,12.5,12.4,14.3,13.8,12.9,12.4]
+# temp_new=[]
+# count=0
+# sum=0
+# for t in temp:
+#     print(t,end=',')
+
+#     if count == 5: 
+#         temp_new.append('N/A')
+#     else:
+#         temp_new.append(t)
+#         sum += t
+#     count +=1
+# print()
+# for t in temp_new:
+#     print(t,end=',')
+# print()
+# print('平均気温は{}度です'.format(sum/count))
 temp=[7.8,9.1,10.2,11.0,12.5,12.4,14.3,13.8,12.9,12.4]
-temp_new=[]
-count=0
-sum=0
 for t in temp:
     print(t,end=',')
-
-    if count == 5: 
-        temp_new.append('N/A')
-    else:
-        temp_new.append(t)
-        sum += t
-    count +=1
 print()
+temp_new=temp[:]
+temp_new[5]='N/A'
+samples=[]
 for t in temp_new:
     print(t,end=',')
+    if not isinstance(t,float):
+        continue
+    samples.append(t)
 print()
-print('平均気温は{}度です'.format(sum/count))
+print(f'平均気温:{sum(samples)/len(samples)}')
--- day0106/ex4_6.py
+++ day0106/ex4_6.py
@@ -1,16 +1,32 @@
+# numbers=[1,1]
+# ratios=[]
+# ratio=0
+# while True:
+#     add = numbers[len(numbers)-1] + numbers[len(numbers)-2]
+#     if add < 1000:
+#        numbers.append(add)
+#        ratio = add/numbers[len(numbers)-2]
+#        ratios.append(ratio)
+#     else:
+#        break
+# print('ratios mae:'+str(ratios))
+# print('numbers:'+str(numbers))
+# for n in range(len(ratios)):
+#     ratios[n] = int(ratios[n]*1000)/1000
+# print('ratios ato:'+str(ratios))
 numbers=[1,1]
-ratios=[]
-ratio=0
 while True:
-    add = numbers[len(numbers)-1] + numbers[len(numbers)-2]
-    if add < 1000:
-       numbers.append(add)
-       ratio = add/numbers[len(numbers)-2]
-       ratios.append(ratio)
-    else:
-       break
-print('ratios mae:'+str(ratios))
-print('numbers:'+str(numbers))
-for n in range(len(ratios)):
-    ratios[n] = int(ratios[n]*1000)/1000
-print('ratios ato:'+str(ratios))
+    n=numbers[-2]+numbers[-1]
+    if n >= 1000:
+        break
+    numbers.append(n)
+print(numbers)
+
+ratios=[]
+for i in range(1,len(numbers)):
+    ratios.append(numbers[i]/numbers[i-1])
+print(ratios)
+
+for i in range(len(ratios)):
+    ratios[i]=int(ratios[i] * 1000) / 1000
+print(ratios)
