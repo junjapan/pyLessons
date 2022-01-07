19baee755c11f887cd73e174d9be7cd1d7c92468
jun <watanabejapan2trap@gmail.com>
2022-01-07 13:17:27 +0900

    c5_22,24,29.py追加 デフォルト引数,キーワード引数,可変長引数

---
+++ day0107/c5_22.py
@@ -0,0 +1,10 @@
+def eat(breakfast,lunch,dinner='カレー'):
+    print('朝は{}を食べました'.format(breakfast))
+    print('昼は{}を食べました'.format(lunch))
+    print('晩は{}を食べました'.format(dinner))
+
+eat('トースト','おにぎり')
+eat('納豆ごはん','ラーメン')
+eat('トースト','おにぎり')
+eat('バナナ','そば','焼肉')
+eat('サンドイッチ','しゅうまい弁当')
---
+++ day0107/c5_24.py
@@ -0,0 +1,8 @@
+def eat(breakfast,lunch='ラーメン',dinner='カレー'):
+    print('朝は{}を食べました'.format(breakfast))
+    print('昼は{}を食べました'.format(lunch))
+    print('晩は{}を食べました'.format(dinner))
+
+eat(breakfast='納豆ごはん',lunch='パン',dinner='カレーうどん')
+eat(dinner='カレーうどん',breakfast='納豆ごはん')
+eat('納豆ごはん',dinner='カレーうどん')
